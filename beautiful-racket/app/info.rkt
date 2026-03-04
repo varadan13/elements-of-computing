69d4f4d225da7fc35f6a1fd5669de11d30dbe758
varadan13 <rcaesar1996@gmail.com>
2026-03-04 14:12:27 +0530

    init(app): initialize beautiful-racket application

---
+++ beautiful-racket/app/info.rkt
@@ -0,0 +1,5 @@
+#lang info
+
+(define collection "beautiful-racket")
+(define deps '("base"))
+(define build-deps '("rackunit-lib"))
---
+++ beautiful-racket/app/main.rkt
@@ -0,0 +1,4 @@
+#lang racket
+
+(module+ main
+  (displayln "beautiful-racket"))
